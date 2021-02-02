c5d4f02c4ec6d18ea4e197e987c203ff8895fcf2
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-02 17:21:18 +0530

    CRUD: Starting work on "Create User"

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-// import { PrismaClient } from "@prisma/client";
 const { PrismaClient } = require("@prisma/client");
+const express = require("express");
 
+const port = process.env.PORT || 5000;
 const prisma = new PrismaClient();
 
-async function main() {
-  const users = await prisma.users.findMany();
-  const posts = await prisma.posts.findMany();
+const app = express();
+app.use(express.json());
 
-  console.log({ users, posts });
-}
+app.post("/users", async function createUser(req, res) {
+  const { name, email, role } = req.body;
 
-main();
+  try {
+    const user = await prisma.user.create({
+      data: { name, email, role },
+    });
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Something went wrong.",
+    });
+  }
+});
+
+app.listen(port, function bootApp() {
+  console.log(`Server running on port ${port}`);
+});
